52abb22057fa689f8757bfd354cf62456e7c00eb
Alex Ceng <izengjing.cn@gmail.com>
2021-03-06 21:42:41 +0800

    auto load api document page in browser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser')
 const port = 3000
 const host = 'localhost'
+const openApiDocs = require('./utils/openApiDocs')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -17,4 +18,7 @@ require('./routes/default')(app)
 require('./routes/more')(app)
 require('./routes/upload')(app)
 
-app.listen(port, host, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, host, () => {
+  openApiDocs(`http://${host}:${port}/api-docs`)
+  console.log(`Example app listening on port ${port}!`)
+});
